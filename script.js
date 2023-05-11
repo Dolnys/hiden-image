e6893e83af680fd72332a64c83dd2bbd8661569e
Dolnys <thedolnys@gmail.com>
2023-05-11 18:21:50 +0200

    final commit

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const img = document.querySelector('.item1');
 
 const showImg = () => {
   img.classList.toggle('hide');
+
+  if (img.classList.contains('hide')) {
+    arrowIcon.style.transform = 'rotate(180deg)';
+  } else {
+    arrowIcon.style.transform = 'rotate(0)';
+  }
 };
 
 arrowBtn.addEventListener('click', showImg);
